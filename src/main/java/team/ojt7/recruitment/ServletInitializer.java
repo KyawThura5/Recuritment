087a68524e939e2dbbadabe69150308ce97e7cc1
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-08 14:39:27 +0630

    first commit

---
+++ src/main/java/team/ojt7/recruitment/ServletInitializer.java
@@ -0,0 +1,13 @@
+package team.ojt7.recruitment;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RecruitmentManagementSystemApplication.class);
+	}
+
+}
---
+++ src/test/java/team/ojt7/recruitment/RecruitmentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package team.ojt7.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
