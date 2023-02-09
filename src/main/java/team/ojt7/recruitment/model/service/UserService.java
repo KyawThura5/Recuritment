2ca8134e2ddf690805cc52d67501fdd549b14e44
khinphyowai99 <59276235+khinphyowai99@users.noreply.github.com>
2023-02-09 15:49:58 +0630

    dispaly user

--- src/main/java/team/ojt7/recruitment/model/service/UserService.java
+++ src/main/java/team/ojt7/recruitment/model/service/UserService.java
@@ -3,10 +3,13 @@ package team.ojt7.recruitment.model.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import team.ojt7.recruitment.model.dto.UserDto;
 import team.ojt7.recruitment.model.entity.User;
 import team.ojt7.recruitment.model.entity.User.Role;
 
+@Service
 public interface UserService {
     public List<UserDto> search(String keyword, Role role);
     public Optional<UserDto> findById(Long id);
