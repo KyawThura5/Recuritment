ee77268bd5404c9da3ea9078c6a99e0676852b22
sandar <sandarsandar421@gmail.com>
2023-02-09 12:57:43 +0630

    rename controller

--- src/main/java/team/ojt7/recruitment/controller/UserController.java
+++ src/main/java/team/ojt7/recruitment/controller/UserController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class UserContrller {
+public class UserController {
 
 	@GetMapping("/admin/user")
 	public String loginview() {
