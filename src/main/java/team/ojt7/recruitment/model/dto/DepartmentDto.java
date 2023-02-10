694413cd55578296245a101bc1f3f46c39fb1130
khinphyowai99 <59276235+khinphyowai99@users.noreply.github.com>
2023-02-10 11:22:51 +0630

    department and user detail

---
+++ src/main/java/team/ojt7/recruitment/model/dto/DepartmentDto.java
@@ -0,0 +1,20 @@
+package team.ojt7.recruitment.model.dto;
+
+public class DepartmentDto {
+
+	private Long id;
+	private String name;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
