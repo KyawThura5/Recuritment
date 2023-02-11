269c912f7cefe593836c5c966496535f958b74d9
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-11 14:13:16 +0630

    fix gender

--- src/main/java/team/ojt7/recruitment/model/entity/Gender.java
+++ src/main/java/team/ojt7/recruitment/model/entity/Gender.java
@@ -3,7 +3,7 @@ package team.ojt7.recruitment.model.entity;
 public enum Gender {
 	MALE("Male"),
 	FEMALE("Female"),
-	OTHER("Others");
+	OTHERS("Others");
 	private String displayName;
 	Gender(String displayName){
 		this.displayName=displayName;
