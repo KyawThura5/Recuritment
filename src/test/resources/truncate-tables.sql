812e58f4dc62cb6f66837131129476ba3d5652c7
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-14 11:28:05 +0630

    implement test cases for position repo

--- src/test/resources/truncate-tables.sql
+++ src/test/resources/truncate-tables.sql
@@ -4,4 +4,5 @@ TRUNCATE TABLE `department`;
 TRUNCATE TABLE `recruitment_resource`;
 TRUNCATE TABLE `direct_recruitment_resource`;
 TRUNCATE TABLE `external_recruitment_resource`;
+TRUNCATE TABLE `position`;
 SET foreign_key_checks = 1;
