bb12944aa81a70ffa54406a77dffc18786a7cfe1
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-14 10:55:05 +0630

    implement test cases for user repo

---
+++ src/test/resources/pre-insert-recruitment-resources-1.sql
@@ -0,0 +1,8 @@
+INSERT INTO `recruitment_resource` (`code`, `name`, `entity_type`, `is_deleted`) VALUES
+('rr001', 'Job.com', 'ExternalRecruitmentResource', false),
+('rr002', 'Job Net', 'ExternalRecruitmentResource', false),
+('rr003', 'Connect Job', 'ExternalRecruitmentResource', true),
+('rr004', '24 HR', 'ExternalRecruitmentResource', false),
+('rr005', '4UJob', 'ExternalRecruitmentResource', true),
+('rr006', 'ACE Inspiration', 'DirectRecruitmentResource', true),
+('rr007', 'Internship', 'DirectRecruitmentResource', false);
