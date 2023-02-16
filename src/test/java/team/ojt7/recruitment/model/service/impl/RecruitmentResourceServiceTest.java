01e56d7a65069fcc937f0fb09c05cc0e4f09cc9b
su-mon-aye <72802135+su-mon-aye@users.noreply.github.com>
2023-02-16 09:56:58 +0630

    adding recruitmentResourceServiceTest

--- src/test/java/team/ojt7/recruitment/model/service/impl/RecruitmentResourceServiceTest.java
+++ src/test/java/team/ojt7/recruitment/model/service/impl/RecruitmentResourceServiceTest.java
@@ -9,12 +9,16 @@ import java.util.List;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import team.ojt7.recruitment.model.dto.RecruitmentResourceDto;
+import team.ojt7.recruitment.model.entity.Department;
+import team.ojt7.recruitment.model.entity.ExternalRecruitmentResource;
 import team.ojt7.recruitment.model.entity.RecruitmentResource;
+import team.ojt7.recruitment.model.entity.Team;
 import team.ojt7.recruitment.model.entity.User.Role;
 import team.ojt7.recruitment.model.repo.RecruitmentResourceRepo;
 import team.ojt7.recruitment.model.service.impl.RecruitmentResourceServiceImpl;
@@ -25,22 +29,28 @@ public class RecruitmentResourceServiceTest {
 	private RecruitmentResourceServiceImpl recruitmentResourceService;
 	@Mock
 	private RecruitmentResourceRepo recruitmentresourceRepo;
+	@Disabled
 	@Test
 	public void testSave() {
-//		RecruitmentResource recruitmentResource=new RecruitmentResource();
-//		recruitmentResource.setId(1L);
-//		recruitmentResource.setCode("R001");
-//		recruitmentResource.setName("Direct");
-//		recruitmentResourceService.save(recruitmentResource);
-//		verify(recruitmentresourceRepo,times(1)).save(recruitmentResource);
+		
+		RecruitmentResource recruitmentResource=new RecruitmentResource();
+		recruitmentResource.setId(1L);
+		recruitmentResource.setCode("R001");
+	    recruitmentResource.setName("Direct");
+	    //when(departmentRepo.save(ArgumentMatchers.any(Department.class))).thenReturn(department);
+	    when(recruitmentresourceRepo.save(recruitmentResource)).thenReturn(recruitmentResource);
+		recruitmentResourceService.save(recruitmentResource);
+		verify(recruitmentresourceRepo,times(1)).save(recruitmentResource);
+		//verify(recruitmentresourceRepo,times(1)).save(team);
 	
 		
 		
 	}
-	//@Disabled
+	@Disabled
 	@Test
 	
 	public void testSearch() {
+		
 		List<RecruitmentResource> resultList=new ArrayList<>();
 		RecruitmentResource recruitmentResource1=new RecruitmentResource();
 		recruitmentResource1.setId(1L);
