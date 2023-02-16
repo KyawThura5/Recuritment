eab4385fd2a308cdfcb0696297f575b000cd291c
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-16 14:22:50 +0630

    remove in memory authentication

--- src/main/java/team/ojt7/recruitment/config/SecurityConfig.java
+++ src/main/java/team/ojt7/recruitment/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -33,16 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	auth.inMemoryAuthentication()
-    		.passwordEncoder(passwordEncoder())
-    		.withUser(
-				User.builder()
-				.username("admin")
-				.password("admin")
-				.roles("ADMIN")
-				.authorities("ADMIN")
-				.build()
-    		);
     	
         auth.jdbcAuthentication()
             .passwordEncoder(passwordEncoder())
