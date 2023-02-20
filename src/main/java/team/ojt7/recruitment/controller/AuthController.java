d0a059ffc3e1a52a123557bb1a1be5e38e936b8e
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-02-20 15:59:46 +0630

    fix views

--- src/main/java/team/ojt7/recruitment/controller/AuthController.java
+++ src/main/java/team/ojt7/recruitment/controller/AuthController.java
@@ -1,13 +1,18 @@
 package team.ojt7.recruitment.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
 
 	@GetMapping("/login")
-	public String showLoginPage() {
+	public String showLoginPage(@RequestParam String error ,ModelMap model) {
+		if (error != null) {
+			model.put("loginFormErrorMessage", "Invalid employee code or password");
+		}
 		return "login";
 	}
 }
