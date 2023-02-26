8710e3cca759bd5436af16452ef957d2bfcff864
su-mon-aye <72802135+su-mon-aye@users.noreply.github.com>
2023-02-27 00:15:54 +0630

    adding deleteDialogBox

---
+++ src/main/resources/static/resources/js/dialogBox.js
@@ -0,0 +1,17 @@
+
+
+// When the user clicks the button, open the modal 
+function showDialog(id) {
+	
+  var modelElement=document.getElementById("modal"+id);
+ modelElement.style.display = "block";
+ 
+	}
+	function closeDialog(id) {
+	
+  var modelElement=document.getElementById("modal"+id);
+ modelElement.style.display = "none";
+}
+
+
+
