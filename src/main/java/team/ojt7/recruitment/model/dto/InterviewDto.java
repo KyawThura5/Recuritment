f72e6220a8eadd2ff21f55cfb393bccc4a2b68a2
KyawThura5 <103934926+KyawThura5@users.noreply.github.com>
2023-02-28 11:04:50 +0630

    controller

--- src/main/java/team/ojt7/recruitment/model/dto/InterviewDto.java
+++ src/main/java/team/ojt7/recruitment/model/dto/InterviewDto.java
@@ -3,11 +3,14 @@ package team.ojt7.recruitment.model.dto;
 import java.util.List;
 import java.util.Objects;
 
+import javax.validation.constraints.NotBlank;
+
 import team.ojt7.recruitment.model.entity.Interview;
 
 public class InterviewDto {
 
 	private Long id;
+	@NotBlank(message="{NotBlank.interview.name")
 	private String name;
 	public Long getId() {
 		return id;
