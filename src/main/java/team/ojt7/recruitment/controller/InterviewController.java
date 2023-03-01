baac937dd6f97e833bcbad58b68bf7ac7d12a349
su-mon-aye <72802135+su-mon-aye@users.noreply.github.com>
2023-03-01 15:18:15 +0630

    adding interviewController

--- src/main/java/team/ojt7/recruitment/controller/InterviewController.java
+++ src/main/java/team/ojt7/recruitment/controller/InterviewController.java
@@ -1,11 +1,20 @@
 package team.ojt7.recruitment.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import team.ojt7.recruitment.model.dto.InterviewDto;
+import team.ojt7.recruitment.model.dto.InterviewSearch;
 import team.ojt7.recruitment.model.service.InterviewService;
+import team.ojt7.recruitment.model.service.exception.InvalidField;
+import team.ojt7.recruitment.model.service.exception.InvalidFieldsException;
 
 @Controller
 public class InterviewController {
@@ -14,22 +23,57 @@ public class InterviewController {
 	private InterviewService interviewService;
 	
 	@GetMapping("/interview/search")
-	public String search() {
+	public String searchInterviews(@ModelAttribute("interviewSearch")
+	InterviewSearch interviewSearch,
+	ModelMap model) {
+		Page<InterviewDto> page = interviewService.search(interviewSearch);
+		model.put("interviewPage", page);
+		model.put("interviewSearch", interviewSearch);
 		return "interviews";
 	}
 	
 	@GetMapping("/interview/edit")
-	public String edit() {
+	public String editInterview(
+			@RequestParam(required = false)
+			Long id,
+			ModelMap model
+			)  {
+		InterviewDto interviewDto = interviewService.findById(id).orElse(new InterviewDto());
+		model.put("interview", interviewDto);
+		String title = interviewDto.getId() == null ? "Add New Interview" : "Edit Interview";
+		model.put("title", title);
+		
 		return "edit-interview";
 	}
 	
 	@PostMapping("/interview/save")
-	public String save() {
+	public String saveInterview(@ModelAttribute("interview") @Validated InterviewDto dto,BindingResult bs,ModelMap model) {
+		
+		if (!bs.hasErrors()) {
+			try {
+				interviewService.save(InterviewDto.parse(dto));
+			} catch (InvalidFieldsException e) {
+				for (InvalidField invalidField : e.getFields()) {
+					bs.rejectValue(invalidField.getField(), invalidField.getCode(), invalidField.getMessage());
+				}
+			}
+		}
+		
+		if(bs.hasErrors()) {
+			String title = dto.getId() == null ? "Add New Interview" : "Edit Interview";
+			model.put("title", title);
+			return "edit-interview";
+		}
+		
 		return "redirect:/interview/search";
 	}
+
+
 	
 	@PostMapping("/interview/delete")
-	public String delete() {
+	public String deleteInterview(@RequestParam("id") Long id) {
+		
+		interviewService.deleteById(id);
 		return "redirect:/interview/search";
 	}
 }
