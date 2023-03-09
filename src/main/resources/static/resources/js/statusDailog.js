249d8ec28a53a8e0c51429e10c99635c997f6110
KyawThura5 <103934926+KyawThura5@users.noreply.github.com>
2023-03-09 15:42:39 +0630

    interview ui

--- src/main/resources/static/resources/js/statusDailog.js
+++ src/main/resources/static/resources/js/statusDailog.js
@@ -1,17 +1,49 @@
-// When the user clicks the button, open the modal 
-function showStatusDialog(id) {
-	
-   var modelElement=document.getElementById("modal1"+id);
-   modelElement.style.display = "block";
-   window.onclick = function(event) {
-    if (event.target == modelElement) {
-       modelElement.style.display = "none";
-    }
-    
+function showChangeStatusDialog(id) {
+
+    $.ajax({
+        type: "GET",
+        url: "/interview/status/change/api?id=" + id,
+        success: function (data) {
+            console.log(data);
+            
+            let form = $("#changeStatusForm");
+            let applicantIdInput = form.find("input[name='id']");
+            applicantIdInput.val(data.id);
+            
+
+            let statusSelect = form.find("select[name='status']");
+            statusSelect.val(data.status);
+
+            let remarkTextarea = form.find("textarea[name='comment']");
+            remarkTextarea.val("");
+
+            let modelElement=document.getElementById("changeStatusModal");
+            modelElement.style.display = "block";
+            window.onclick = function(event) {
+                if (event.target == modelElement) {
+                    modelElement.style.display = "none";
+                }
+            }
+
+            let selectedStatus = data.status;
+            let btnSubmit = $('#btnStatusSubmit');
+            btnSubmit.addClass("disabled");
+           $('#status1').on('change', function(event) {
+                let changeValue = this.value;
+                if (changeValue == selectedStatus) {
+                    btnSubmit.addClass("disabled");
+                } else {
+                    btnSubmit.removeClass("disabled");
+                }
+            });
+        },
+        error: function (e) {
+            console.log('error');
+            console.log(e);
+        }
+    });
 }
- 
-	}
-function closeStatusDialog(id) {
-   var modelElement=document.getElementById("modal1"+id);
-   modelElement.style.display = "none";
+function closeModal(id) {
+    let modelElement=document.getElementById(id);
+    modelElement.style.display = "none";
 }
