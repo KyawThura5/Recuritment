dd057d6d566fc336f1b7e744ef8443011530cdb0
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-03-12 04:30:03 +0630

    implement view for interview calendar for home

--- src/main/java/team/ojt7/recruitment/model/dto/HomeDto.java
+++ src/main/java/team/ojt7/recruitment/model/dto/HomeDto.java
@@ -9,6 +9,8 @@ public class HomeDto {
 	private long jobExpiringsCount;
 	private long notStartedInterviewsCount;
 	private long newApplicantsCount;
+	private Map<Integer, Integer> newInterviewsCountByDate;
+	private Map<Integer, Integer> pastInterviewsCountByDate;
 	private Map<RecruitmentResourceDto, Long> topRecruitmentResources = new HashMap<>();
 
 	public long getJobOpeningsCount() {
@@ -41,6 +43,22 @@ public class HomeDto {
 
 	public void setNewApplicantsCount(long newApplicantsCount) {
 		this.newApplicantsCount = newApplicantsCount;
+	}
+
+	public Map<Integer, Integer> getNewInterviewsCountByDate() {
+		return Map.of(12, 1, 15, 2, 22, 1, 30, 3);
+	}
+
+	public void setNewInterviewsCountByDate(Map<Integer, Integer> newInterviewsCountByDate) {
+		this.newInterviewsCountByDate = newInterviewsCountByDate;
+	}
+
+	public Map<Integer, Integer> getPastInterviewsCountByDate() {
+		return Map.of(2, 1, 12, 2, 20, 1, 30, 1, 31, 2);
+	}
+
+	public void setPastInterviewsCountByDate(Map<Integer, Integer> pastInterviewCountByDate) {
+		this.pastInterviewsCountByDate = pastInterviewCountByDate;
 	}
 
 	public Map<RecruitmentResourceDto, Long> getTopRecruitmentResources() {
---
+++ src/main/resources/static/resources/js/calendar.js
@@ -0,0 +1,106 @@
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
+
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
+
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    selectYear.value = year;
+    selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+        // creates a table row
+        let row = document.createElement("tr");
+
+        //creating individual cells, filing them up with data.
+        for (let j = 0; j < 7; j++) {
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {
+                break;
+            }
+
+            else {
+                let cell = document.createElement("td");
+                cell.height = 100;
+                cell.width = "14.28%";
+                cell.style.minHeight = 100;
+                let cellText = document.createTextNode(date);
+                let dateDiv = document.createElement("div");
+                dateDiv.innerText = date;
+                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                    cell.classList.add("bg-info");
+                } // color today's date
+                cell.appendChild(dateDiv);
+
+                let newInt = document.getElementById("newInterviewByDate" + date);
+                if (newInt) {
+                    let newIntCount = newInt.innerText;
+                    let div = document.createElement("label");
+                    div.classList.add("badge", "bg-success", "mx-1")
+                    div.innerText = newIntCount;
+                    cell.appendChild(div);
+                }
+
+                let pastInt = document.getElementById("pastInterviewByDate" + date);
+                if (pastInt) {
+                    let pastIntCount = pastInt.innerText;
+                    let div = document.createElement("label");
+                    div.classList.add("badge", "bg-secondary", "mx-1")
+                    div.innerText = pastIntCount;
+                    cell.appendChild(div);
+                }
+                
+                
+                row.appendChild(cell);
+                date++;
+            }
+
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
+    }
+
+}
