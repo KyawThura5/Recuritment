ca56eeaad92473e971d0f76687f8776dc1f44cb5
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-03-13 13:09:49 +0630

    report view

--- src/main/java/team/ojt7/recruitment/controller/ReportController.java
+++ src/main/java/team/ojt7/recruitment/controller/ReportController.java
@@ -1,18 +1,27 @@
 package team.ojt7.recruitment.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import team.ojt7.recruitment.model.service.ReportService;
 
 @Controller
 public class ReportController {
+	
+	@Autowired
+	private ReportService reportService;
 
-	@GetMapping("/report/{name}")
+	@GetMapping("/report")
 	public String showReport(
-			@PathVariable
-			String name
+			@RequestParam
+			String name,
+			ModelMap model
 			) {
 		if ("topRecruitmentResources".equals(name)) {
+			model.put("reports", reportService.searchTopRecruitmentResources());
 			return "top-recruitment-resources";
 		} else if ("topRecruitmentResourcesByPosition".equals(name)) {
 			return "top-recruitment-resources-by-position";
