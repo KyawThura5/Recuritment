c037cf9134b835173fe4fe577cd035dd5eda97d9
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2023-03-28 12:50:39 +0630

    update candidate and interview

--- src/main/java/team/ojt7/recruitment/model/dto/ApplicantDto.java
+++ src/main/java/team/ojt7/recruitment/model/dto/ApplicantDto.java
@@ -212,7 +212,7 @@ public class ApplicantDto {
 	public boolean isAvailableForNewInterview() {
 		return getStatus().getStep() > 2
 				&& (getLastestInterview() == null || getLastestInterview().getStatus().getStep() > 2)
-				&& getStatus().getStep() < Status.JOB_OFFERED.getStep();
+				&& getStatus().getStep() < Status.REJECTED.getStep();
 	}
 
 	public boolean isUpdatableStatus() {
